d09d3c08369e6af7be7a488a76bfc907746e1c24
vvchebotar <v.v.chebotar@yandex.ru>
2018-07-01 23:08:31 +0300

    added id to model

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,16 +5,26 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal {
+    private long id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
+    public Meal(long id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        id = 0L;
     }
 
     public LocalDateTime getDateTime() {
@@ -35,5 +45,9 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public long getId() {
+        return id;
     }
 }
--- src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -3,6 +3,8 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
 public class MealWithExceed {
+    private long id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -11,11 +13,40 @@ public class MealWithExceed {
 
     private final boolean exceed;
 
+    public MealWithExceed(long id, LocalDateTime dateTime, String description, int calories, boolean exceed) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.exceed = exceed;
+    }
+
     public MealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.exceed = exceed;
+        id = 0L;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExceed() {
+        return exceed;
     }
 
     @Override
